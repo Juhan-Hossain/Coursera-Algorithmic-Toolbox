66fe123e77d2aa0c3bec35e259a1606dc3d2a262
Juhan Hossain <juhanhossain@gmail.com>
2020-05-06 10:06:54 +0600

    Add files via upload

---
+++ programming challenges/week 2/gcd of large no.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll gcd(ll a,ll b)
+{
+    if(b==0)
+        return a;
+    //else
+        return gcd(b,a%b);
+}
+int main()
+{
+    ll a,b;
+    cin>>a>>b;
+
+    cout<<gcd(b,a)<<"\n";
+
+    return 0;
+}
+
